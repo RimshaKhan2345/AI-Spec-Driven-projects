84c0b070ac0a2142cb106f9213c4ad0919efe0d8
Ramisha Tariq <ramishatariq13@gmail.com>
2026-02-06 01:43:59 +0500

    Update database URL and secret key placeholders

--- full stack todo app/config.py
+++ full stack todo app/config.py
@@ -5,10 +5,10 @@ from typing import Optional
 
 class Settings(BaseSettings):
     # Database settings
-    DATABASE_URL: Optional[str] = "postgresql://neondb_owner:npg_P46QVlmBqxDj@ep-wispy-fire-ahgr18i5-pooler.c-3.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
+    DATABASE_URL: Optional[str] = "paste neon db string key"
     
     # Authentication settings
-    SECRET_KEY: str = "napi_3ggx0n6twt43yz9km4dphptemhrgvbp25d100cc807fnajmnf1pi6vl1rr7u5nzt"
+    SECRET_KEY: str = "paste your secret key here"
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
     
